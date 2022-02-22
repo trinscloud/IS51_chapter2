3f126aa014e48283dcf1389b4c94ba142f583fde
trinscloud <trinhamm@gmail.com>
2022-02-22 14:16:53 -0800

    initial commit

---
+++ chapter2.py
@@ -0,0 +1,24 @@
+# print(3+2,3-2,3*2)
+# print(8/2,8**2,2*(3+4))
+
+# speed = 50
+# timeElasped = 14
+# distance = speed * timeElasped
+# print(distance)
+
+
+# a = 2
+# b = 3
+# print(abs(1-(4*b)))
+# print(int((a ** b) + .8))
+# print(round(a/b,3))
+
+# totalInches = 41
+# feet = totalInches // 12
+# inches = totalInches % 12
+# print(feet,inches)
+
+
+
+
+
